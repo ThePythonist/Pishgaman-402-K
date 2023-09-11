9f29df7ced93317ca1a1d87e018a2ce346e2c7a5
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-09-11 20:20:43 +0330

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,2 @@
+name = input("Enter your name : ")
+print("hello",name,1,2,3,4,5,6)
---
+++ 3.py
@@ -0,0 +1,6 @@
+zel = 6
+mohit = zel * 4
+masahat = zel ** 2
+#print(mohit,masahat)
+print("mohit :",mohit)
+print("masahat :",masahat)
